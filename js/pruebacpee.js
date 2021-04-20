5150657fd413e5c8a4f21c3d44c77ff648516772
JerogZurc <jorgecruzprof@gmail.com>
2021-04-20 13:02:39 -0500

    Create pruebacpee.js

---
+++ js/pruebacpee.js
@@ -0,0 +1,14 @@
+var  MIAPLICACION = MIAPLICACION || {}; //Objeto global que servira como contenedor de atributos, metodos y funciones
+// tambien se le conoce como espacio de nombres
+MIAPLICACION.event = { //subespacios de nombre para encapsular funciones para determinadas tareas
+  accion1: funcion(atributo1, atrubuto2){ //nombre: (atributos) { funcion }
+    //codigo de relleno
+  }, //listado de funcion separandolas con coma
+  accion2: funcion(atributo1, atributo2){
+    //codigo de relleno
+  }
+} ;
+MIAPLICACION.event.acion1("elemento1", "elemento2"); //para usar objeto.subojeto.funcion("elemento")
+//Paradigma de objetos
+
+//Uso de Frameworks
